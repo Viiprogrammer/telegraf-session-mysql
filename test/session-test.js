72998ea67eeb91980da184e692920ab2644db825
Maxim Telegin <m_telega@mail.ru>
2021-10-08 00:37:20 +0300

    Added connect method to tests

--- test/session-test.js
+++ test/session-test.js
@@ -11,75 +11,91 @@ const options = {
 describe('Telegraf Session', function () {
   it('should retrieve and save session', function (done) {
     const mySQLSession = new MySQLSession(options)
+
     const userID = 1
     const chatID = 2
-    mySQLSession.getSession(userID, chatID)
-      .then((session) => {
-        should.exist(session)
-        session.foo = 42
-        return mySQLSession.saveSession(userID, chatID, session)
-      })
+    mySQLSession.connect()
       .then(() => {
-        return mySQLSession.getSession(userID, chatID)
+        mySQLSession.getSession(userID, chatID)
+          .then((session) => {
+            should.exist(session)
+            session.foo = 42
+            return mySQLSession.saveSession(userID, chatID, session)
+          })
+          .then(() => {
+            return mySQLSession.getSession(userID, chatID)
+          })
+          .then((session) => {
+            should.exist(session)
+            session.should.be.deepEqual({ foo: 42 })
+          })
+          .then(done)
       })
-      .then((session) => {
-        should.exist(session)
-        session.should.be.deepEqual({ foo: 42 })
-      })
-      .then(done)
   })
 
   it('should be defined', function (done) {
     const app = new Telegraf()
     const session = new MySQLSession(options)
-    app.on('text',
-      session.middleware(),
-      (ctx) => {
-        should.exist(ctx.session)
-        ctx.session.foo = 42
-        done()
+    session.connect()
+      .then(() => {
+        app.on('text',
+          session.middleware(),
+          (ctx) => {
+            should.exist(ctx.session)
+            ctx.session.foo = 42
+            done()
+          })
+        app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
       })
-    app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
   })
 
   it('should handle existing session', function (done) {
     const app = new Telegraf()
     const session = new MySQLSession(options)
-    app.on('text',
-      session.middleware(),
-      (ctx) => {
-        should.exist(ctx.session)
-        ctx.session.should.have.property('foo')
-        ctx.session.foo.should.be.equal(42)
-        done()
+    session.connect()
+      .then(() => {
+        app.on('text',
+          session.middleware(),
+          (ctx) => {
+            should.exist(ctx.session)
+            ctx.session.should.have.property('foo')
+            ctx.session.foo.should.be.equal(42)
+            done()
+          })
+        app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
       })
-    app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
   })
 
   it('should handle not existing session', function (done) {
     const app = new Telegraf()
     const session = new MySQLSession(options)
-    app.on('text',
-      session.middleware(),
-      (ctx) => {
-        should.exist(ctx.session)
-        ctx.session.should.not.have.property('foo')
-        done()
+    session.connect()
+      .then(() => {
+        app.on('text',
+          session.middleware(),
+          (ctx) => {
+            should.exist(ctx.session)
+            ctx.session.should.not.have.property('foo')
+            done()
+          })
+        app.handleUpdate({message: {chat: {id: 2}, from: {id: 999}, text: 'hey'}})
       })
-    app.handleUpdate({message: {chat: {id: 2}, from: {id: 999}, text: 'hey'}})
   })
 
   it('should handle session reset', function (done) {
     const app = new Telegraf()
     const session = new MySQLSession(options)
-    app.on('text',
-      session.middleware(),
-      (ctx) => {
-        ctx.session = null
-        should.exist(ctx.session)
-        ctx.session.should.not.have.property('foo')
-        done()
+    session.connect()
+      .then(() => {
+        app.on('text',
+          session.middleware(),
+          (ctx) => {
+            ctx.session = null
+            should.exist(ctx.session)
+            ctx.session.should.not.have.property('foo')
+            done()
+          })
+        app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
       })
-    app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
   })
 })
